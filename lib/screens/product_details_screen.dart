05e06ccf3568b0d0174fdd21bae768b2a4d5ddfc
Mukandila Mukandila <mmukandila@intelcomexpress.com>
2022-10-04 13:32:18 -0400

    adding navigation to app

---
+++ lib/screens/product_details_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ProductDetailsScreen extends StatelessWidget {
+  static const routeName = '/product-details';
+
+  @override
+  Widget build(BuildContext context) {
+    final productId = ModalRoute.of(context).settings.arguments as String;
+    return Scaffold(
+      appBar: AppBar(title: Text(productId)),
+    );
+  }
+}
