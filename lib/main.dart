a4b5df97ef030cd5631fcf554d12b197383b4c6b
Mukandila Mukandila <mmukandila@intelcomexpress.com>
2022-10-04 19:54:35 -0400

    Working with providers and listeners

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_complete_guide/screens/product_details_screen.dart';
-import '/screens/products_overview_screen.dart';
+import 'package:provider/provider.dart';
+
+import './screens/product_details_screen.dart';
+import './screens/products_overview_screen.dart';
+import './providers/products_provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-          colorScheme: ThemeData().colorScheme.copyWith(
-                primary: Colors.purple,
-                secondary: Colors.deepOrange,
-              ),
-          fontFamily: 'Lato'),
-      home: ProductsOverviewScreen(),
-      routes: {
-        ProductDetailsScreen.routeName: (ctx) => ProductDetailsScreen(),
-      },
+    return ChangeNotifierProvider(
+      create: (_) => Products(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+            colorScheme: ThemeData().colorScheme.copyWith(
+                  primary: Colors.purple,
+                  secondary: Colors.deepOrange,
+                ),
+            fontFamily: 'Lato'),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailsScreen.routeName: (ctx) => ProductDetailsScreen(),
+        },
+      ),
     );
   }
 }
