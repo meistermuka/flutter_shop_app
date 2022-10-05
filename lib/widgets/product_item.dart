a63833111023cc10f949a0772d15535b3dc89d3d
Mukandila Mukandila <mmukandila@intelcomexpress.com>
2022-10-05 09:43:40 -0400

    Consumer vs Provider of

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -7,7 +7,7 @@ import '../screens/product_details_screen.dart';
 class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(context, listen: false);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -22,13 +22,16 @@ class ProductItem extends StatelessWidget {
             )),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: IconButton(
-            icon: Icon(
-                product.isFavourite ? Icons.favorite : Icons.favorite_outline),
-            color: Theme.of(context).colorScheme.secondary,
-            onPressed: () {
-              product.toggleFavouriteStatus();
-            },
+          leading: Consumer<Product>(
+            builder: (ctx, product, _) => IconButton(
+              icon: Icon(product.isFavourite
+                  ? Icons.favorite
+                  : Icons.favorite_outline),
+              color: Theme.of(context).colorScheme.secondary,
+              onPressed: () {
+                product.toggleFavouriteStatus();
+              },
+            ),
           ),
           trailing: IconButton(
             icon: Icon(Icons.shopping_cart),
