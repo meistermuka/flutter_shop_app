e9e762db15a026af0f438cac88723045ee0d6f06
Mukandila Mukandila <mmukandila@intelcomexpress.com>
2022-10-04 20:10:04 -0400

    Listening in different places and ways

--- lib/screens/product_details_screen.dart
+++ lib/screens/product_details_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/providers/products_provider.dart';
+import 'package:provider/provider.dart';
 
 class ProductDetailsScreen extends StatelessWidget {
   static const routeName = '/product-details';
@@ -6,8 +8,13 @@ class ProductDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String;
+    final specificProduct = Provider.of<Products>(
+      context,
+      listen:
+          false, // This will not rebuild as it is not an active listener. Getting data one time.
+    ).findById(productId);
     return Scaffold(
-      appBar: AppBar(title: Text(productId)),
+      appBar: AppBar(title: Text(specificProduct.title)),
     );
   }
 }
